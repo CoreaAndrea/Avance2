932622205f1b40ce4fd16c8a0e045c5d5e8fd690
Colegio <Colegio@LAPTOP-H7896TCR>
2025-03-17 21:08:05 -0600

    Avance9

--- src/avanceandrea2/Historial.java
+++ src/avanceandrea2/Historial.java
@@ -1,7 +1,57 @@
 
 package avanceandrea2;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import javax.swing.JOptionPane;
+
 public class Historial {
+    public void mostrarHistorial(Instructor instructor) {
+        String historial = "Instructores seleccionados:\n";
+        for (Instructor selectedInstructor : instructor.getI_Seleccionado()) {
+            historial += "Instructor: " + selectedInstructor.getInstructor() + 
+                         " - Día: " + selectedInstructor.getDia() + 
+                         " - Hora: " + selectedInstructor.getHora() + "\n";
+        }
+        JOptionPane.showMessageDialog(null, historial);
+    }
+
     
+    public void mostrarRutina(Crear_Rutina crearRutina) {
+        if (crearRutina != null) {
+            crearRutina.mostrarRutina();  
+        } else {
+            JOptionPane.showMessageDialog(null, "No se ha creado ninguna rutina.");
+        }
+    }
+
     
-}
+    public void guardarHistorial(Crear_Rutina crearRutina, Instructor instructor) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("historial.historial"))) {
+            oos.writeObject(crearRutina);
+            oos.writeObject(instructor);
+            JOptionPane.showMessageDialog(null, "Historial y rutina guardados correctamente en historial.historial.");
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Error al guardar el historial.");
+        }
+    }
+
+    
+    public void cargarHistorial() {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("historial.historial"))) {
+            Crear_Rutina rutinaCargada = (Crear_Rutina) ois.readObject();
+            Instructor instructorCargado = (Instructor) ois.readObject();
+
+            JOptionPane.showMessageDialog(null, "Datos cargados correctamente.");
+            mostrarRutina(rutinaCargada);
+            mostrarHistorial(instructorCargado);
+
+        } catch (IOException | ClassNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "No se encontraron datos guardados.");
+        }
+    }
+}  
+
--- src/avanceandrea2/Main.java
+++ src/avanceandrea2/Main.java
@@ -1,10 +1,109 @@
 
 package avanceandrea2;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
     public static void main(String[] args) {
-       
+        Usuario usuario = new Usuario();  
+        Historial historial = new Historial();
+        
+        
+        usuario.archivo();  
+        
+        
+        boolean accesoConcedido = false;
+        while (!accesoConcedido) {
+            String menuUsuario = "1. Crear cuenta\n"
+                               + "2. Iniciar sesión\n"
+                               + "3. Salir\n";
+            
+            int opcionUsuario = Integer.parseInt(JOptionPane.showInputDialog(menuUsuario + "Seleccione una opción:"));
+            switch (opcionUsuario) {
+                case 1:
+                    usuario.crear_cuenta();  
+                    break;
+                case 2:
+                    accesoConcedido = usuario.login(); 
+                    if (accesoConcedido) {
+                        JOptionPane.showMessageDialog(null, "Inicio de sesión exitoso.");
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Credenciales incorrectas.");
+                    }
+                    break;
+                case 3:
+                    JOptionPane.showMessageDialog(null, "Saliendo del sistema.");
+                    System.exit(0);  
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opción inválida.");
+            }
+        }
+        Crear_Rutina crearRutina = new Crear_Rutina(); 
+
+        Instructor instructor = new Instructor();
+
+        
+        String menu = "1. Ver ejercicios\n"
+                    + "2. Ejercicios de resistencia\n"
+                    + "3. Ejercicios para masa muscular\n"
+                    + "4. Crear rutina personalizada\n"
+                    + "5. Ver instructores\n" 
+                    + "5. Ver Historial\n" 
+                    + "6. Salir\n";
+
+        boolean continuar = true;
+        while (continuar) {
+            int opcion = Integer.parseInt(JOptionPane.showInputDialog(menu + "Seleccione una opción:"));
+            switch (opcion) {
+                case 1:
+                    crearRutina.verEjercicios();  
+                    break;
+                case 2:
+                    crearRutina.resistencia();  
+                    break;
+                case 3:
+                    crearRutina.masa_muscular();  
+                    break;
+                case 4:
+                    crearRutina.verEjercicios1(); 
+                    break;
+                case 5:
+                    
+                    String submenuInstructores = "1. Ver información de instructores\n"
+                                                + "2. Seleccionar instructores\n"
+                                                + "3. Salir\n";
+                    int opcionInstructor = Integer.parseInt(JOptionPane.showInputDialog(submenuInstructores + "Seleccione una opción:"));
+                    switch (opcionInstructor) {
+                        case 1:
+                            instructor.verInformacion(); 
+                            break;
+                        case 2:
+                            instructor.elegir();  
+                            break;
+                        case 3:
+                            break;
+                        default:
+                            JOptionPane.showMessageDialog(null, "Opción inválida.");
+                    }
+                    break;
+                
+                case 6:
+                    historial.mostrarHistorial(instructor);
+                    historial.mostrarRutina(crearRutina);
+                    historial.guardarHistorial(crearRutina, instructor);
+                    historial.cargarHistorial();
+                    break;
+                
+                case 7:
+                    continuar = false;  
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opción inválida.");
+            }
+        }
     }
-    
 }
+       
+    
